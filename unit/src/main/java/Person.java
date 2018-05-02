a5af14c0a204d50b21d3e14f1957c49fea3029af
Irindul <mathieu.regnard2@gmail.com>
2018-05-02 11:25:09 +0200

    Person reformated with 2 spaces for tab (personnal choice)

--- unit/src/main/java/Person.java
+++ unit/src/main/java/Person.java
@@ -2,55 +2,58 @@ import java.util.Objects;
 
 public class Person {
 
-    private String name;
+	private String name;
 
-    private Vector position;
+	private Vector position;
 
-    public Person(String name, Vector position) {
-        this.name = name;
-        this.position = position;
-    }
+	public Person(String name, Vector position) {
+		this.name = name;
+		this.position = position;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public Vector getPosition() {
-        return position;
-    }
+	public Vector getPosition() {
+		return position;
+	}
 
-    public void setPosition(Vector position) {
-        this.position = position;
-    }
+	public void setPosition(Vector position) {
+		this.position = position;
+	}
 
-    public void move(Vector vector) {
-        this.position = position.add(vector);
-    }
+	public void move(Vector vector) {
+		this.position = position.add(vector);
+	}
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", position=" + position +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Person{" +
+			"name='" + name + '\'' +
+			", position=" + position +
+			'}';
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return Objects.equals(name, person.name) &&
-                Objects.equals(position, person.position);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Person person = (Person) o;
+		return Objects.equals(name, person.name) &&
+			Objects.equals(position, person.position);
+	}
 
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(name, position);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, position);
+	}
 }
