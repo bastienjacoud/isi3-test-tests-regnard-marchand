91165a47464aca031c288494da5f24eefaf29a6f
Irindul <mathieu.regnard2@gmail.com>
2018-05-02 10:25:13 +0200

    First exercice worst solution ever but tests are passing

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,23 +1,56 @@
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static java.util.stream.Collectors.toList;
-
 public class FizzBuzz {
 
-    private IntStream integerList;
+	private IntStream integerList;
 
-    public FizzBuzz(Integer length) {
-        assert length >= 0 : "length should not be negative";
-        integerList = IntStream.rangeClosed(0, length - 1);
-    }
+	public FizzBuzz(Integer length) {
+		assert length >= 0 : "length should not be negative";
+		integerList = IntStream.rangeClosed(0, length - 1);
+	}
 
-    public IntStream getIntegerList() {
-        return integerList;
-    }
+	public IntStream getIntegerList() {
+		return integerList;
+	}
 
 
-    public List<String> fizzBuzz() {
-        return integerList.mapToObj(String::valueOf).collect(toList());
-    }
+	public List<String> fizzBuzz() {
+		return integerList.mapToObj(this::getStringValue).collect(toList());
+	}
+
+	private String getStringValue(int number) {
+		boolean isMultipleOfThree = isMultipleOf(3, number);
+		boolean isMultipleOfFive = isMultipleOf(5, number);
+		boolean isMultipleOfFifteen = isMultipleOfFive && isMultipleOfThree;
+		boolean isMultipleOfSeven = isMultipleOf(7, number);
+
+		if (number == 42) {
+			return "The answer to life, the universe and everything";
+		}
+
+		if (isMultipleOfThree) {
+			return "Fizz";
+		}
+
+		if (isMultipleOfFifteen) {
+			return "Fizz Buzz";
+		}
+
+		if (isMultipleOfFive) {
+			return "Buzz";
+		}
+
+		if (isMultipleOfSeven) {
+			return "It's a trap";
+		}
+
+		return String.valueOf(number);
+	}
+
+	private boolean isMultipleOf(int multiple, int evaluatedNumber) {
+		return evaluatedNumber % multiple == 0;
+	}
 }
--- fizzbuzz/src/test/java/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/FizzBuzzTest.java
@@ -1,28 +1,90 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 @DisplayName("Fizz Buzz Tests 😇")
 class FizzBuzzTest {
 
-    private FizzBuzz main;
+	private FizzBuzz main;
+
+	@BeforeEach
+	void setUp() {
+		main = new FizzBuzz(100);
+	}
+
+	@ParameterizedTest
+	@DisplayName("should have list of integer of size length")
+	@ValueSource(ints = {0, 1, 10, 100})
+	void testListBuild(int length) {
+		main = new FizzBuzz(length);
+		assertEquals(main.getIntegerList().count(), length);
+	}
+
+	@Test
+	@DisplayName("should verify length positiv sign")
+	void testNegativeLength() {
+		assertThrows(AssertionError.class, () -> main = new FizzBuzz(-1));
+	}
+
+	@ParameterizedTest
+	@DisplayName("should return string value of these numbers")
+	@ValueSource(ints = {1, 2, 4})
+	void testStringValueOfNumbers(int number) {
+		List<String> fizzBuzz = main.fizzBuzz();
+		assertEquals(fizzBuzz.get(number), String.valueOf(number));
+	}
+
+	@Test
+	@DisplayName("should return Fizz for 0")
+	void testZero() {
+		testAssertEquals("Fizz", 0);
+	}
+
+	@ParameterizedTest
+	@DisplayName("should verify multiple of three are replaced by Fizz")
+	@ValueSource(ints = {3, 6, 9})
+	void testMultipleThree(int multipleOfThree) {
+		testAssertEquals("Fizz", multipleOfThree);
+	}
+
+	@ParameterizedTest
+	@DisplayName("should verify multiple of five are replaced by Buzz")
+	@ValueSource(ints = {5, 10, 20})
+	void testMultipleFive(int multipleOfFive) {
+		testAssertEquals("Buzz", multipleOfFive);
+	}
+
+	@ParameterizedTest
+	@DisplayName("should verify multiple of 3 and five are replace by Fizz")
+	@ValueSource(ints = {15, 30, 45})
+	void testMultipleOfThreeAndFive(int multiple) {
+		testAssertEquals("Fizz", multiple);
+	}
+
+	@ParameterizedTest
+	@DisplayName("should verify multiple of 7 are replaced by It's a trap")
+	@ValueSource(ints = {7, 14, 28})
+	void testMultipleOfSeven(int multiple) {
+		testAssertEquals("It's a trap", multiple);
+	}
+
+	@Test
+	@DisplayName("should return The answer to life, the universe and everything for 42")
+	void testFourtyTwo() {
+		testAssertEquals("The answer to life, the universe and everything", 42);
+	}
 
 
-    @ParameterizedTest
-    @DisplayName("should have list of integer of size length")
-    @ValueSource(ints = { 0, 1, 10, 100 })
-    void testListBuild(int length) {
-        main = new FizzBuzz(length);
-        assertEquals(main.getIntegerList().count(), length);
-    }
-
-    @Test
-    @DisplayName("should verify length positiv sign")
-    void testNegativeLength() {
-        assertThrows(AssertionError.class, () -> main = new FizzBuzz(-1));
-    }
+	// TODO: 02/05/2018 Better name
+	private void testAssertEquals(String expected, int number) {
+		List<String> fizzBuzz = main.fizzBuzz();
+		assertEquals(expected, fizzBuzz.get(number));
+	}
 }
